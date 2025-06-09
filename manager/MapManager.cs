ef4739591b04875ab1f1bc334417a6c0e54b8792
owner <>
2025-06-09 20:52:49 +0900

    사용하지 않는 namespace 삭제

--- manager/MapManager.cs
+++ manager/MapManager.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 
 namespace game
 {
